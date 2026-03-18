42d8232534c47f02f853c2f40d48e96d40ca42ad
rendezvxus <realsuppaplex@gmail.com>
2026-03-18 14:31:52 +0300

    header + footer

--- src/components/main-components/Footer.tsx
+++ src/components/main-components/Footer.tsx
@@ -1,7 +1,19 @@
 export default function Footer() {
+    
+    const linkRef = "https://vistegra.by/"
+
     return (
         <footer>
-            <p>Footer</p>
+            <p>© 2020-2026, made with </p>
+            <img 
+                className="footer-heart" 
+                src="/Heart.svg"
+                alt="<3"
+                />
+            <p> by </p>
+            <a href={linkRef}>
+                Vistegra
+            </a>
         </footer>
     )
 }
--- src/components/main-components/Header.tsx
+++ src/components/main-components/Header.tsx
@@ -1,7 +1,13 @@
 export default function Header() {
     return (
         <header>
-            <p>Header</p>
+            <div className="header-wrapper">
+                <div className="logo-img-wrapper">
+                    <img src="/V.svg" alt="VISTEGRA" />
+                    <a className="logo-img-text-container">VISTEGRA</a>
+                </div>
+                <h1 className="header-pagename">- Shop</h1>
+            </div>
         </header>
     )
 }
