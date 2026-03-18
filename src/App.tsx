e8c2cb301c23dbce61b0548a02b6576bbed63a23
rendezvxus <realsuppaplex@gmail.com>
2026-03-18 14:02:00 +0300

    Revision of knowledge

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header, Main, Footer } from './components/main-components/index.ts'
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer /> 
+    </> 
+  )
+}
