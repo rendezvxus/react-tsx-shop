47979f839cde2903ac0a9aaf2b3e9438a9d0f3be
rendezvxus <realsuppaplex@gmail.com>
2026-03-19 14:23:46 +0300

    deploy (probably not last)

--- src/components/catalog/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ItemsList } from './ItemsList.tsx'
-export { default as Item } from './Item.tsx'
-export { default as Cart } from './Cart.tsx'
-export { default as Filter } from './Filter.tsx'
--- src/components/main-components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header.tsx'
-export { default as Main } from './Main.tsx'
-export { default as Footer } from './Footer.tsx'
